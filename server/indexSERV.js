619dd53950eb6a241d95bc063c7a9989a2679d22
unknown <waly@gmail.com>
2022-04-23 14:07:54 +0200

    simple serv config

---
+++ server/indexSERV.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
