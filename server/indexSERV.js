390eadb592c4bf2cec2ec516b9062f1bfb2f1d2d
unknown <waly@gmail.com>
2022-04-24 16:33:11 +0200

    initial data + serv config

--- server/indexSERV.js
+++ server/indexSERV.js
@@ -1,11 +1,11 @@
+process.env.PWD = process.cwd()
 const path = require('path');
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../coctails-frontend/build')));
+app.use(express.static(process.env.PWD  + '/coctails-frontend/src/data/images'));
 
 
 app.get('/', (req, res) => {
