63d80528c2c2ae0c2a0a8d32b99f7d978f7c050c
unknown <waly@gmail.com>
2022-04-24 16:04:04 +0200

    initial data

--- server/indexSERV.js
+++ server/indexSERV.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../coctails-frontend/build')));
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../coctails-frontend/build', 'index.html'));})
 
 app.listen(PORT, () => {
