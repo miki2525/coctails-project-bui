7e0490fc5ec9d48e8ca4684f1ed1a71c8a036431
unknown <waly@gmail.com>
2022-05-03 15:17:13 +0200

    button showDetails

--- coctails-frontend/src/components/coctail.js
+++ coctails-frontend/src/components/coctail.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/coctail.scss'
 
 export default function Coctail({id, name, image, type, glass, ratings, ingredients, steps, comments}) {
-
+    const [showDetails, setVisibility] = useState(false);
 
     return (
         <section className="coctail">
             <img className="imgCoctail" src={image}/>
             <h1>{name}</h1>
             <h3>Typ: {type}</h3>
+            <button className="btn btn-info" onClick={() => setVisibility(!showDetails)}>SZCZEGÓŁY</button>
+            {showDetails && <>
+                <IngredientsList list={ingredients} />
+                {/*<Instructions steps={steps} />*/}
+            </>}
 
-            {/*///TODO button on click show the rest*/}
-            {/*<IngredientsList list={ingredients} />*/}
-            {/*<Instructions steps={steps} />*/}
         </section>
     );
 }
